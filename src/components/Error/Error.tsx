f0f88230e03fd55f04a939defa868751ff77a519
Amirhosein Torabi <torabi46@gmail.com>
2024-05-05 11:32:16 +0330

    create new components

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,13 @@
+export default function ErrorMessage(props: { error: string }) {
+  const { error } = props;
+
+  return (
+    <>
+      <p className="error">
+        {" "}
+        <span>⛔</span>
+        {error}
+      </p>
+    </>
+  );
+}
